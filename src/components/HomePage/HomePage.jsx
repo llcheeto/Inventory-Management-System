05e267c365e4e5837a49b1c77d39d779dd98cd19
LLCheeto <89275143+llcheeto@users.noreply.github.com>
2022-10-04 16:04:11 -0400

    poral homepage bones

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import "./homepage.scss";
+import Navbar from "../Navbar/Navbar";
+import Arcade from "../Arcade/Arcade.jsx";
+import Inventory from "../Inventory/Inventory.jsx";
+import NewInventory from "../NewInventory/NewInventory.jsx";
+import TimeSheet from "../TimeSheet/TimeSheet.jsx";
+import POS from "../POS/POS.jsx";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const componentList = [Arcade, Inventory, NewInventory, TimeSheet, { POS }];
+
+  return (
+    <div id="portal">
+      <Navbar />
+      <div>
+        <div className="portal--container"></div>
+      </div>
+    </div>
+  );
 }
